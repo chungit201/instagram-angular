9f343d86a102c040d9de9b259472637da87d9f89
Tran Anh Quang <quangtaph12673@fpt.edu.vn>
2021-09-19 21:39:22 +0700

    update mvc

--- backend/app.js
+++ backend/app.js
@@ -20,5 +20,5 @@ app.use(express.json());
 
 const port = process.env.PORT || 8000
 app.listen(port, () => {
-    console.log(`Server is runing on port : ${port}`);
+    console.log(`Server is running on port : ${port}`);
 })
